cddcb3747ee61dae36a2b353cef23e1ec0566a8e
theCount <thescheremetev@mail.ru>
2021-03-09 23:59:39 +0300

    mend

--- script/script.js
+++ script/script.js
@@ -9,3 +9,25 @@ emptyLinks.forEach( (item) => {
     });
 });
 
+// menu
+const subMenu = document.querySelector('.submenu'),
+      subMenuItem = subMenu.querySelectorAll('.submenu__item');
+
+subMenu.addEventListener('mouseover', () => {
+    subMenu.classList.add('activ');
+    subMenuItem.forEach( (item) => {
+        item.classList.add('activ');
+    });
+});
+subMenu.addEventListener('mouseout', () => {
+    subMenu.classList.remove('activ');
+    subMenuItem.forEach( (item) => {
+        item.classList.remove('activ');
+    });
+});
+
+// modal
+const modalWindow = document.querySelectorAll('.modal'),
+      modalContent = modalWindow.querySelectorAll('.modal__content'),
+      modalBtnClose = modalContent.querySelectorAll('.modal__close');
+
