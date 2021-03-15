d5b30c4a4186ca69322a84a22bb309b7dd61c325
theCount <thescheremetev@mail.ru>
2021-03-16 00:52:05 +0300

    I add section promo delivery and footer by he not responsive

--- script/script.js
+++ script/script.js
@@ -26,26 +26,38 @@ subMenu.addEventListener('mouseout', () => {
     });
 });
 
+// Burger menu icon
+const burgerMenuIcon = document.querySelector('.burger-icon'),
+      burgerMenuIconLine = burgerMenuIcon.querySelector('.burger-line');
+
+burgerMenuIcon.addEventListener('click', () => {
+    burgerMenuIcon.classList.toggle('activ');
+    burgerMenuIconLine.classList.toggle('activ');
+});
+
 // modal 
 const btnCloseModal = document.querySelectorAll('[data-close]'),
       btnOpenModal = document.querySelectorAll('[data-modal-btn]'),
-      BtnOpenModalLogin = document.querySelector('[data-modal-btn="user"]'),
+      BtnOpenModalLogin = document.querySelectorAll('[data-modal-btn="user"]'),
       linkRegisterUser = document.querySelector('.login-in__reg-link'),
       modal = document.querySelectorAll('[data-modal]'),
       modalUser = document.querySelector('[data-modal="user"]'),
       modalRegister = document.querySelector('[data-modal="register"]'),
       modalSubscribe = document.querySelector('[data-modal="subscribe"]');
 
-BtnOpenModalLogin.addEventListener('click', ()=>{
-    modalUser.classList.add('show');
-    modalUser.classList.remove('hide');
-    document.body.style.overflow = 'hidden';
-})
+BtnOpenModalLogin.forEach( item => {
+    item.addEventListener('click', ()=>{
+        modalUser.classList.add('show');
+        modalUser.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    });
+});      
 
 function closeModal() {
     modal.forEach( item => {
         item.classList.remove('show');
         item.classList.add('hide');
+        document.body.style.overflow = '';
     });
 }
 
@@ -72,7 +84,7 @@ linkRegisterUser.addEventListener('click', () =>{
     closeModal();
     modalRegister.classList.remove('hide');
     modalRegister.classList.add('show');
-})
+});
 
 
 // disable btn registration, subscribe
@@ -93,12 +105,5 @@ linkModalSubscribe.addEventListener('click', (e) =>{
     e.preventDefault();
     modalSubscribe.classList.remove('hide');
     modalSubscribe.classList.add('show');
-
-})
-
-
-// swiper navigation
-let swiper = new Swiper('.swiper-container', {
-   slidesPerView: 3,
+    document.body.style.overflow = 'hidden';
 });
-  
